67b7264a2bb41f6b1c7a46aa08f1d444321b4009
alex91-html <alex91-html@users.noreply.github.com>
2025-03-10 18:50:13 +0100

    updated HTML, JS, css and fixed problem with layout

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # js-project-recipe-library
 
 to do:
-[] js filtering 
-[]
-[]
-[] 
+[X] insert recepie cards 
+[X] Update the HTML 
+[X] Update the JavaScript
+[X] Update the CSS 
+[X] fix problem layout 
+[] get placeholder images?
+[] filter amongst the recipes in the array
+[] sort the recipes in the array
+[] get a random recipe from the array
+[] ...
--- script.js
+++ script.js
@@ -161,85 +161,107 @@ const recipes = [
 ]
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const recipeContainer = document.getElementById('recipeAPI');
+  const messageSection = document.getElementById('message-section');
+  const checkboxes = document.querySelectorAll('.filter-group input[type="checkbox"]');
+  const radios = document.querySelectorAll('.sort-group input[type="radio"]');
 
 
+  const loadRecipes = () => {
+    recipeContainer.innerHTML = '';
 
+    recipes.forEach(recipe => {
+      const ingredientsList = recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
 
+      const recipeCard = `
+        <a href="#">
+          <article class="recipe-card">
+            <img src="${recipe.image}" alt="${recipe.title}">
+            <h2 class="title">${recipe.title}</h2>
+            <hr class="divider">
+            <div class="recipe-info">
+              <div class="info-item">
+                <span class="label">Cuisine: </span>
+                <span class="value">${recipe.cuisine}</span>
+              </div>
+              <div class="info-item">
+                <span class="label">Time:</span>
+                <span class="value">${recipe.readyInMinutes} minutes</span>
+              </div>
+              <div class="info-item">
+                <span class="label">Servings:</span>
+                <span class="value">${recipe.servings} minutes</span>
+              </div>
+            </div>
+            <hr class="divider">
+            <div class="ingredients">
+              <h3>Ingredients:</h3>
+              <ul>${ingredientsList}</ul>
+            </div>
+          </article>
+        </a>
+      `;
 
+      recipeContainer.innerHTML += recipeCard;
+    });
+  };
 
+  // here starts the magic, the function that will update the message
+  // based on the user's selections
+  const updateMessage = () => {
+    let message = '';
 
+    const isAllSelected = document.getElementById('filter-all').checked;
 
+    if (isAllSelected) {
+      message = 'You greedy pig!';
+      clearSelections();
+    } else {
 
-
-
-
-
-
-
-
-
-
-
-
-
-const messageSection = document.getElementById('message-section');
-const checkboxes = document.querySelectorAll('.filter-group input[type="checkbox"]');
-const radios = document.querySelectorAll('.sort-group input[type="radio"]');
-
-// here starts the magic, the function that will update the message
-// based on the user's selections
-const updateMessage = () => {
-  let message = '';
-
-  const isAllSelected = document.getElementById('filter-all').checked;
-
-  if (isAllSelected) {
-    message = 'You greedy pig!';
-    clearSelections();
-  } else {
-
-    if (document.getElementById('filter-vegan').checked) {
-      message += 'Oh I see,what about the fish and some good tasty egss from time to time? Are you sure you are not Vegetarian? Maybe?';
+      if (document.getElementById('filter-vegan').checked) {
+        message += 'Oh I see,what about the fish and some good tasty egss from time to time? Are you sure you are not Vegetarian? Maybe?';
+      }
+      if (document.getElementById('filter-vegetarian').checked) {
+        message += '<br><br>Good choice, better cutting on all that meat!';
+      }
+      if (document.getElementById('filter-gluten-free').checked) {
+        message += '<br><br>I\'m with you, gluten it\'s poisoned!';
+      }
+      if (document.getElementById('filter-dairy-free').checked) {
+        message += '<br><br>hmm are you sure cheese is something you can fully give up?';
+      }
+      if (document.getElementById('sort-shortest').checked) {
+        message += '<br><br>Trying to impress someone with little time?';
+      } else if (document.getElementById('sort-longest').checked) {
+        message += '<br><br>Do you really want to spend hours in the kitchen?';
+      }
     }
-    if (document.getElementById('filter-vegetarian').checked) {
-      message += '<br><br>Good choice, better cutting on all that meat!';
-    }
-    if (document.getElementById('filter-gluten-free').checked) {
-      message += '<br><br>I\'m with you, gluten it\'s poisoned!';
-    }
-    if (document.getElementById('filter-dairy-free').checked) {
-      message += '<br><br>hmm are you sure cheese is something you can fully give up?';
-    }
-    if (document.getElementById('sort-shortest').checked) {
-      message += '<br><br>Trying to impress someone with little time?';
-    } else if (document.getElementById('sort-longest').checked) {
-      message += '<br><br>Do you really want to spend hours in the kitchen?';
-    }
-  }
 
-  messageSection.innerHTML = message || 'Hey hey hey welcome!';
-};
-// here it ends
+    messageSection.innerHTML = message || 'Hey hey hey welcome!';
+  };
+  // here it ends
 
-const clearSelections = () => {
-  checkboxes.forEach(checkbox => checkbox.checked = false);
-  radios.forEach(radio => radio.checked = false);
-};
+  const clearSelections = () => {
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+    radios.forEach(radio => radio.checked = false);
+  };
 
-const addEventListeners = () => {
-  checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', updateMessage);
-  });
+  const addEventListeners = () => {
+    checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', updateMessage);
+    });
 
-  radios.forEach(radio => {
-    radio.addEventListener('change', updateMessage);
-  });
-};
-
-const init = () => {
-  addEventListeners();
-  updateMessage();
-};
+    radios.forEach(radio => {
+      radio.addEventListener('change', updateMessage);
+    });
+  };
 
 
-init();
+  const init = () => {
+    addEventListeners();
+    updateMessage();
+    loadRecipes();
+  };
+  init();
+});
